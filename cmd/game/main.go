87201c3f539f817bc42cb0e83d3dc5bf43e6cc07
Alfie Hiscox <alfie.hiscox@gmail.com>
2024-10-31 00:25:32 +0000

    randomize ships and also placement verification

--- cmd/game/main.go
+++ cmd/game/main.go
@@ -1,22 +1,40 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-	"math/rand"
+	"math/rand/v2"
+	"strings"
 )
 
 const (
 	BOARD_WIDTH  = 10
 	BOARD_HEIGHT = 10
 
-	MAX_RANDOM_LIMIT = 100
+	MAX_RANDOM_LIMIT = 1000
 
 	HORIZONTAL Orientation = "HORIZONTAL"
 	VERTICAL   Orientation = "VERTICAL"
 )
 
 type Orientation string
+
+// Coordinates are zero based, and therefore
+// - x  is between 0 and BOARD_WIDHT - 1
+// - y  is between 0 and BOARD_HEIGHT - 1
 type Coordinate [2]int
+
+func NewCoordinate(x, y int) (Coordinate, error) {
+	if x < 0 || x >= BOARD_WIDTH {
+		return Coordinate{}, errors.New(fmt.Sprintf("x value %d out of bounds", x))
+	}
+
+	if y < 0 || y >= BOARD_HEIGHT {
+		return Coordinate{}, errors.New(fmt.Sprintf("y value %d out of bounds", y))
+	}
+
+	return Coordinate{x, y}, nil
+}
 
 type Cell struct {
 	occupied bool
@@ -26,7 +44,35 @@ type Cell struct {
 type Board []Cell
 
 func NewBoard() Board {
-	return make(Board, BOARD_HEIGHT*BOARD_WIDTH)
+	board := make(Board, BOARD_HEIGHT*BOARD_WIDTH)
+	for i := range board {
+		board[i].chosen = false
+		board[i].occupied = false
+	}
+	return board
+}
+
+func (b Board) String() string {
+	builder := strings.Builder{}
+	builder.WriteString("\n")
+
+	count := 1
+	for i := range b {
+		if b[i].occupied {
+			builder.WriteString(" X ")
+		} else {
+			builder.WriteString(" O ")
+		}
+
+		if count == BOARD_WIDTH {
+			builder.WriteString("\n")
+			count = 1
+		} else {
+			count += 1
+		}
+	}
+
+	return builder.String()
 }
 
 type Player struct {
@@ -48,30 +94,221 @@ func NewPlayer(name string) *Player {
 }
 
 func get_random_orientation() Orientation {
-	if rand.Intn(2) == 0 {
+	if rand.IntN(2) == 0 {
 		return HORIZONTAL
 	} else {
 		return VERTICAL
 	}
 }
 
+func get_random_coord(size int) Coordinate {
+	x := rand.IntN(BOARD_WIDTH - size)
+	y := rand.IntN(BOARD_HEIGHT - size)
+	//fmt.Printf("Coord{%d,%d} - ", x, y)
+	return Coordinate{x, y}
+}
+
+func (p *Player) place_ship(size int, orientation Orientation, coord Coordinate) error {
+	x, y := coord[0], coord[1]
+	idx := y*BOARD_WIDTH + x
+
+	switch orientation {
+	case HORIZONTAL:
+		for i := 0; i < size; i++ {
+			cell := p.player_board[idx+i]
+			// fmt.Printf("index=%d - cell=%v - ", idx+i, cell)
+			if cell.occupied {
+				msg := fmt.Sprintf("Cell at %v already occupied", coord)
+				//fmt.Print(msg + "\n")
+				return errors.New(msg)
+			}
+		}
+	case VERTICAL:
+		for i := 0; i < size; i++ {
+			cell := p.player_board[idx+(i*BOARD_WIDTH)]
+			// fmt.Printf("index=%d - cell=%v - ", idx+(i*BOARD_WIDTH), cell)
+			if cell.occupied {
+				msg := fmt.Sprintf("Cell at %v already occupied", coord)
+				//fmt.Print(msg + "\n")
+				return errors.New(msg)
+			}
+		}
+	default:
+		msg := fmt.Sprintf("Unknown orientation: %s", orientation)
+		//fmt.Print(msg + "\n")
+		return errors.New(msg)
+	}
+
+	switch orientation {
+	case HORIZONTAL:
+		for i := 0; i < size; i++ {
+			p.player_board[idx+i].occupied = true
+		}
+	case VERTICAL:
+		for i := 0; i < size; i++ {
+			p.player_board[idx+(i*BOARD_WIDTH)].occupied = true
+		}
+	}
+
+	// msg := fmt.Sprintf("Placed Ship: %v [%s]", coord, orientation)
+	//fmt.Println(msg)
+
+	return nil
+}
+
+func verify_coordinate(size int, orientation Orientation, coord Coordinate) error {
+	if coord[0] < 0 || coord[1] < 0 {
+		msg := fmt.Sprintf("Carrier at %v [%s] is off the board", coord, orientation)
+		//fmt.Print(msg + "\n")
+		return errors.New(msg)
+	}
+
+	if coord[0] > BOARD_WIDTH || coord[1] > BOARD_HEIGHT {
+		msg := fmt.Sprintf("Carrier at %v [%s] is off the board", coord, orientation)
+		//fmt.Print(msg + "\n")
+		return errors.New(msg)
+	}
+
+	if orientation == HORIZONTAL && coord[0] > BOARD_WIDTH-size {
+		msg := fmt.Sprintf("Carrier at %v [%s] is off the board", coord, orientation)
+		//fmt.Print(msg + "\n")
+		return errors.New(msg)
+	}
+
+	if orientation == VERTICAL && coord[1] > BOARD_HEIGHT-size {
+		msg := fmt.Sprintf("Carrier at %v [%s] is off the board", coord, orientation)
+		//fmt.Print(msg + "\n")
+		return errors.New(msg)
+	}
+
+	return nil
+}
+
+func (p *Player) randomize_ship_placement(size int) error {
+
+	orientation := get_random_orientation()
+	coord := get_random_coord(size)
+
+	var err error
+	switch size {
+	case 2:
+		err = p.place_destroyer(orientation, coord)
+	case 3:
+		err = p.place_cruiser_or_submarine(orientation, coord)
+	case 4:
+		err = p.place_battleship(orientation, coord)
+	case 5:
+		err = p.place_carrier(orientation, coord)
+	default:
+		return errors.New("Unknown ship with unknown size")
+	}
+
+	attempt := 0
+	for err != nil {
+
+		orientation := get_random_orientation()
+		coord := get_random_coord(size)
+
+		switch size {
+		case 2:
+			err = p.place_destroyer(orientation, coord)
+		case 3:
+			err = p.place_cruiser_or_submarine(orientation, coord)
+		case 4:
+			err = p.place_battleship(orientation, coord)
+		case 5:
+			err = p.place_carrier(orientation, coord)
+		default:
+			return errors.New("Unknown ship with unknown size")
+		}
+
+		if attempt == MAX_RANDOM_LIMIT {
+			return errors.New("Max random limit reached. Cannot place ship!")
+		} else {
+			attempt += 1
+		}
+	}
+
+	return nil
+}
+
 // Places ships on player_board in random fashion
-func (p *Player) randomize_placement() {}
+func (p *Player) randomize_placement() error {
+
+	if err := p.randomize_ship_placement(5); err != nil {
+		return err
+	}
+
+	if err := p.randomize_ship_placement(4); err != nil {
+		return err
+	}
+
+	if err := p.randomize_ship_placement(3); err != nil {
+		return err
+	}
+
+	if err := p.randomize_ship_placement(3); err != nil {
+		return err
+	}
+
+	if err := p.randomize_ship_placement(2); err != nil {
+		return err
+	}
+
+	return nil
+}
 
 // Places 5 square ship on player_board. Errors if invalid placement.
-func (p *Player) place_carrier(orientation Orientation, coord Coordinate) error { return nil }
+func (p *Player) place_carrier(orientation Orientation, coord Coordinate) error {
+	if err := verify_coordinate(5, orientation, coord); err != nil {
+		return err
+	}
+
+	if err := p.place_ship(5, orientation, coord); err != nil {
+		return err
+	}
+
+	return nil
+}
 
 // Places 4 square ship on player_board. Errors if invalid placement.
-func (p *Player) place_battleship(orientation Orientation, coord Coordinate) error { return nil }
+func (p *Player) place_battleship(orientation Orientation, coord Coordinate) error {
+	if err := verify_coordinate(4, orientation, coord); err != nil {
+		return err
+	}
+
+	if err := p.place_ship(4, orientation, coord); err != nil {
+		return err
+	}
+
+	return nil
+}
 
 // Places 3 square ship on player_board. Errors if invalid placement.
-func (p *Player) place_cruiser(orientation Orientation, coord Coordinate) error { return nil }
+func (p *Player) place_cruiser_or_submarine(orientation Orientation, coord Coordinate) error {
+	if err := verify_coordinate(3, orientation, coord); err != nil {
+		return err
+	}
 
-// Places 3 square ship on player_board. Errors if invalid placement.
-func (p *Player) place_submarine(orientation Orientation, coord Coordinate) error { return nil }
+	if err := p.place_ship(3, orientation, coord); err != nil {
+		return err
+	}
+
+	return nil
+}
 
 // Places 2 square ship on player_board. Errors if invalid placement.
-func (p *Player) place_destroyer(orientation Orientation, coord Coordinate) error { return nil }
+func (p *Player) place_destroyer(orientation Orientation, coord Coordinate) error {
+	if err := verify_coordinate(2, orientation, coord); err != nil {
+		return err
+	}
+
+	if err := p.place_ship(2, orientation, coord); err != nil {
+		return err
+	}
+
+	return nil
+}
 
 // Get's a player's coordinate guess checking against their
 // previous turns (i.e. target_board)
@@ -91,15 +328,15 @@ func (p *Player) mark_target_attempt(coordiant Coordinate, hit bool) {}
 func (p *Player) mark_player_attempt(coordiant Coordinate, hit bool) {}
 
 func main() {
-	p1 := Player{name: "player 1"}
+	p1 := NewPlayer("player 1")
 	p1.randomize_placement()
-	p2 := Player{name: "player 2"}
+	p2 := NewPlayer("player 2")
 	p2.randomize_placement()
 
 	var winner *Player
 
-	turn_player := &p1
-	enemy_player := &p2
+	turn_player := p1
+	enemy_player := p2
 
 	for {
 
@@ -118,7 +355,7 @@ func main() {
 	}
 
 	if winner != nil {
-		fmt.Printf("The winner is %d!\n", winner.name)
+		fmt.Printf("The winner is %s!\n", winner.name)
 	}
 }
 
--- cmd/game/main_test.go
+++ cmd/game/main_test.go
@@ -1,6 +1,8 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestPlaceCarrierSuccess(t *testing.T) {
 	player := NewPlayer("test_player")
@@ -8,7 +10,7 @@ func TestPlaceCarrierSuccess(t *testing.T) {
 	coord := Coordinate{2, 2}
 
 	if err := player.place_carrier(orientation, coord); err != nil {
-		t.Fatal("err should be nil")
+		t.Fatalf("err should be nil: %s", err)
 	}
 
 	for i := 0; i < 5; i++ {
@@ -22,7 +24,6 @@ func TestPlaceCarrierSuccess(t *testing.T) {
 }
 
 func TestPlaceCarrierFailure(t *testing.T) {
-
 	player := NewPlayer("test_player")
 	if err := player.place_carrier(HORIZONTAL, Coordinate{0, 1}); err != nil {
 		t.Fatal("failed in set up")
@@ -39,8 +40,8 @@ func TestPlaceCarrierFailure(t *testing.T) {
 		{orientation: HORIZONTAL, coord: Coordinate{-2, 4}},
 
 		// Off the bottom of the grid
-		{orientation: VERTICAL, coord: Coordinate{BOARD_HEIGHT + 2, 2}},
-		{orientation: VERTICAL, coord: Coordinate{BOARD_HEIGHT - 3, 5}},
+		{orientation: VERTICAL, coord: Coordinate{2, BOARD_HEIGHT + 2}},
+		{orientation: VERTICAL, coord: Coordinate{5, BOARD_HEIGHT - 3}},
 		// minus y
 		{orientation: VERTICAL, coord: Coordinate{-2, 8}},
 
@@ -53,7 +54,7 @@ func TestPlaceCarrierFailure(t *testing.T) {
 
 	for _, test := range tests {
 		if err := player.place_carrier(test.orientation, test.coord); err == nil {
-			t.Fatalf("expected error, got nil: %s", test)
+			t.Fatalf("expected error, got nil: %v", test)
 		}
 	}
 }
@@ -64,7 +65,7 @@ func TestPlaceBattleshipSuccess(t *testing.T) {
 	coord := Coordinate{2, 2}
 
 	if err := player.place_battleship(orientation, coord); err != nil {
-		t.Fatal("err should be nil")
+		t.Fatalf("err should be nil: %s", err)
 	}
 
 	for i := 0; i < 4; i++ {
@@ -90,13 +91,13 @@ func TestPlaceBattleshipFailure(t *testing.T) {
 	}{
 		// Off the right side of the grid
 		{orientation: HORIZONTAL, coord: Coordinate{BOARD_WIDTH + 2, 2}},
-		{orientation: HORIZONTAL, coord: Coordinate{BOARD_WIDTH - 3, 5}},
+		{orientation: HORIZONTAL, coord: Coordinate{BOARD_WIDTH - 2, 5}},
 		// minus x
 		{orientation: HORIZONTAL, coord: Coordinate{-2, 4}},
 
 		// Off the bottom of the grid
-		{orientation: VERTICAL, coord: Coordinate{BOARD_HEIGHT + 2, 2}},
-		{orientation: VERTICAL, coord: Coordinate{BOARD_HEIGHT - 3, 5}},
+		{orientation: VERTICAL, coord: Coordinate{2, BOARD_HEIGHT + 2}},
+		{orientation: VERTICAL, coord: Coordinate{5, BOARD_HEIGHT - 2}},
 		// minus y
 		{orientation: VERTICAL, coord: Coordinate{-2, 8}},
 
@@ -109,74 +110,18 @@ func TestPlaceBattleshipFailure(t *testing.T) {
 
 	for _, test := range tests {
 		if err := player.place_battleship(test.orientation, test.coord); err == nil {
-			t.Fatalf("expected error, got nil: %s", test)
+			t.Fatalf("expected error, got nil: %v", test)
 		}
 	}
 }
 
-func TestPlaceCruiserSuccess(t *testing.T) {
+func TestPlaceCruiserOrSubmarineSuccess(t *testing.T) {
 	player := NewPlayer("test_player")
 	orientation := HORIZONTAL
 	coord := Coordinate{2, 2}
 
-	if err := player.place_cruiser(orientation, coord); err != nil {
-		t.Fatal("err should be nil")
-	}
-
-	for i := 0; i < 3; i++ {
-		x := coord[0] + i
-		y := coord[1]
-		cell := player.player_board[y*BOARD_WIDTH+x]
-		if !cell.occupied {
-			t.Fatalf("cell{%d,%d} was meant to be occupied", x, y)
-		}
-	}
-}
-
-func TestPlaceCruiserFailure(t *testing.T) {
-
-	player := NewPlayer("test_player")
-	if err := player.place_cruiser(HORIZONTAL, Coordinate{0, 1}); err != nil {
-		t.Fatal("failed in set up")
-	}
-
-	tests := []struct {
-		orientation Orientation
-		coord       Coordinate
-	}{
-		// Off the right side of the grid
-		{orientation: HORIZONTAL, coord: Coordinate{BOARD_WIDTH + 2, 2}},
-		{orientation: HORIZONTAL, coord: Coordinate{BOARD_WIDTH - 1, 5}},
-		// minus x
-		{orientation: HORIZONTAL, coord: Coordinate{-2, 4}},
-
-		// Off the bottom of the grid
-		{orientation: VERTICAL, coord: Coordinate{BOARD_HEIGHT + 2, 2}},
-		{orientation: VERTICAL, coord: Coordinate{BOARD_HEIGHT - 1, 5}},
-		// minus y
-		{orientation: VERTICAL, coord: Coordinate{-2, 8}},
-
-		// overlapping existing ship
-		{orientation: VERTICAL, coord: Coordinate{0, 0}},
-		{orientation: VERTICAL, coord: Coordinate{2, 1}},
-		{orientation: HORIZONTAL, coord: Coordinate{1, 1}},
-		{orientation: HORIZONTAL, coord: Coordinate{2, 1}},
-	}
-
-	for _, test := range tests {
-		if err := player.place_cruiser(test.orientation, test.coord); err == nil {
-			t.Fatalf("expected error, got nil: %s", test)
-		}
-	}
-}
-
-func TestPlaceSubmarineSuccess(t *testing.T) {
-	player := NewPlayer("test_player")
-	orientation := HORIZONTAL
-	coord := Coordinate{2, 2}
-
-	if err := player.place_submarine(orientation, coord); err != nil {
-		t.Fatal("err should be nil")
+	if err := player.place_cruiser_or_submarine(orientation, coord); err != nil {
+		t.Fatalf("err should be nil: %s", err)
 	}
 
 	for i := 0; i < 3; i++ {
@@ -192,7 +137,7 @@ func TestPlaceSubmarineSuccess(t *testing.T) {
 func TestPlaceSubmarineFailure(t *testing.T) {
 
 	player := NewPlayer("test_player")
-	if err := player.place_submarine(HORIZONTAL, Coordinate{0, 1}); err != nil {
+	if err := player.place_cruiser_or_submarine(HORIZONTAL, Coordinate{0, 1}); err != nil {
 		t.Fatal("failed in set up")
 	}
 
@@ -207,8 +152,8 @@ func TestPlaceSubmarineFailure(t *testing.T) {
 		{orientation: HORIZONTAL, coord: Coordinate{-2, 4}},
 
 		// Off the bottom of the grid
-		{orientation: VERTICAL, coord: Coordinate{BOARD_HEIGHT + 2, 2}},
-		{orientation: VERTICAL, coord: Coordinate{BOARD_HEIGHT - 1, 5}},
+		{orientation: VERTICAL, coord: Coordinate{2, BOARD_HEIGHT + 2}},
+		{orientation: VERTICAL, coord: Coordinate{5, BOARD_HEIGHT - 1}},
 		// minus y
 		{orientation: VERTICAL, coord: Coordinate{-2, 8}},
 
@@ -220,8 +165,8 @@ func TestPlaceSubmarineFailure(t *testing.T) {
 	}
 
 	for _, test := range tests {
-		if err := player.place_submarine(test.orientation, test.coord); err == nil {
-			t.Fatalf("expected error, got nil: %s", test)
+		if err := player.place_cruiser_or_submarine(test.orientation, test.coord); err == nil {
+			t.Fatalf("expected error, got nil: %v", test)
 		}
 	}
 }
@@ -232,7 +177,7 @@ func TestPlaceDestroyerSuccess(t *testing.T) {
 	coord := Coordinate{2, 2}
 
 	if err := player.place_destroyer(orientation, coord); err != nil {
-		t.Fatal("err should be nil")
+		t.Fatalf("err should be nil: %s", err)
 	}
 
 	for i := 0; i < 2; i++ {
@@ -258,13 +203,13 @@ func TestPlaceDestroyerFailure(t *testing.T) {
 	}{
 		// Off the right side of the grid
 		{orientation: HORIZONTAL, coord: Coordinate{BOARD_WIDTH + 2, 2}},
-		{orientation: HORIZONTAL, coord: Coordinate{BOARD_WIDTH - 1, 5}},
+		{orientation: HORIZONTAL, coord: Coordinate{BOARD_WIDTH, 5}},
 		// minus x
 		{orientation: HORIZONTAL, coord: Coordinate{-2, 4}},
 
 		// Off the bottom of the grid
-		{orientation: VERTICAL, coord: Coordinate{BOARD_HEIGHT + 2, 2}},
-		{orientation: VERTICAL, coord: Coordinate{BOARD_HEIGHT - 1, 5}},
+		{orientation: VERTICAL, coord: Coordinate{2, BOARD_HEIGHT + 2}},
+		{orientation: VERTICAL, coord: Coordinate{5, BOARD_HEIGHT}},
 		// minus y
 		{orientation: VERTICAL, coord: Coordinate{-2, 8}},
 
@@ -276,7 +221,51 @@ func TestPlaceDestroyerFailure(t *testing.T) {
 
 	for _, test := range tests {
 		if err := player.place_destroyer(test.orientation, test.coord); err == nil {
-			t.Fatalf("expected error, got nil: %s", test)
+			t.Fatalf("expected error, got nil: %v", test)
 		}
+	}
+}
+
+func TestRandomizePlacementSuccess(t *testing.T) {
+	player := NewPlayer("test_player")
+
+	t.Log(player.player_board)
+
+	if err := player.randomize_placement(); err != nil {
+		t.Fatalf("err should be nil: %s", err)
+	}
+
+	c := 0
+	for _, cell := range player.player_board {
+		if cell.occupied {
+			c += 1
+		}
+	}
+
+	if c != 17 {
+		t.Fatalf("Expected 17 squares to be filled, got=%d", c)
+	}
+}
+
+func TestRandomizePlacementLimit(t *testing.T) {
+	player := NewPlayer("test_player")
+
+	paint_row := true
+	for i := range player.player_board {
+
+		if i%BOARD_WIDTH-1 == 0 {
+			paint_row = !paint_row
+		}
+
+		paint_row = !paint_row
+
+		if paint_row {
+			player.player_board[i].occupied = true
+		}
+
+	}
+
+	if err := player.randomize_placement(); err == nil {
+		t.Fatalf("expected error, got nil")
 	}
 }
